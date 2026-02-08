2e56f73e59b274bf8ba4e297ef692c6bc030b8df
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-02-08 21:35:42 +0600

    env varable conigure done for all files

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import status from 'http-status';
+import AppError from '../errorHelpers/AppError';
 
 
 dotenv.config();
@@ -36,7 +38,7 @@ const loadEnvVariables = (): EnvConfig => {
 
      requireEnvVariable.forEach((variable) => {
           if (!process.env[variable]) {
-               throw new Error(`Environment variable  is required but not set in .env file.`);
+               throw new AppError(status.INTERNAL_SERVER_ERROR, `Environment variable ${variable} is required but not set in .env file.`);
           }
      })
 
