93e67840124e7b46a7ee7c7029c5e05157b1cd92
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-02-09 23:38:26 +0600

    patient register and login part done

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -1,38 +1,62 @@
 import { Request, Response } from "express";
+import status from "http-status";
+import ms, { StringValue } from "ms";
+import { envVars } from "../../config/env";
 import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sendResponse";
+import { tokenUtils } from "../../utils/token";
 import { AuthService } from "./auth.service";
-import status from "http-status";
 
-const registerPatient = catchAsync(
-     async (req: Request, res: Response) => {
-          const payload = req.body;
+const registerPatient = catchAsync(async (req: Request, res: Response) => {
+     const maxAge = ms(envVars.ACCESS_TOKEN_EXPIRES_IN as StringValue);
+     console.log({ maxAge });
+     const payload = req.body;
 
-          console.log(payload);
+     console.log(payload);
 
-          const result = await AuthService.registerPatient(payload);
+     const result = await AuthService.registerPatient(payload);
 
-          sendResponse(res, {
-               httpStatusCode: status.CREATED,
-               success: true,
-               message: "Patient registered successfully",
-               data: result,
-          })
-     }
-)
+     const { accessToken, refreshToken, token, ...rest } = result;
+     //    console.log(result);
 
-const loginUser = catchAsync(
-     async (req: Request, res: Response) => {
-          const payload = req.body;
-          const result = await AuthService.loginUser(payload);
-          sendResponse(res, {
-               httpStatusCode: status.OK,
-               success: true,
-               message: "User logged in successfully",
-               data: result,
-          })
-     }
-)
+     tokenUtils.setAccessTokenCookie(res, accessToken);
+     tokenUtils.setRefreshTokenCookie(res, refreshToken);
+     tokenUtils.setBetterAuthSessionCookie(res, token as string);
+
+     sendResponse(res, {
+          httpStatusCode: status.CREATED,
+          success: true,
+          message: "Patient registered successfully",
+          data: {
+               token,
+               accessToken,
+               refreshToken,
+               ...rest,
+          },
+     });
+});
+
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+     const payload = req.body;
+     const result = await AuthService.loginUser(payload);
+     const { accessToken, refreshToken, token, ...rest } = result;
+
+     tokenUtils.setAccessTokenCookie(res, accessToken);
+     tokenUtils.setRefreshTokenCookie(res, refreshToken);
+     tokenUtils.setBetterAuthSessionCookie(res, token);
+
+     sendResponse(res, {
+          httpStatusCode: status.OK,
+          success: true,
+          message: "User logged in successfully",
+          data: {
+               token,
+               accessToken,
+               refreshToken,
+               ...rest,
+          },
+     });
+});
 
 export const AuthController = {
      registerPatient,
