16f97431a993332748a439a77203f38ed1fbe56c
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-02-12 22:04:30 +0600

    add get me route

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -4,138 +4,167 @@ import AppError from "../../errorHelpers/AppError";
 import { auth } from "../../lib/auth";
 import { prisma } from "../../lib/prisma";
 import { tokenUtils } from "../../utils/token";
+import { IRequestUser } from "../../interface/requestUser.interface";
 
 interface IRegisterPatientPayload {
-    name: string;
-    email: string;
-    password: string;
+     name: string;
+     email: string;
+     password: string;
 }
 
 const registerPatient = async (payload: IRegisterPatientPayload) => {
-    const { name, email, password } = payload;
+     const { name, email, password } = payload;
 
-    const data = await auth.api.signUpEmail({
-        body: {
-            name,
-            email,
-            password,
-            //default values
-            // needsPasswordChange: false,
-            // role: Role.PATIENT
-        }
-    })
+     const data = await auth.api.signUpEmail({
+          body: {
+               name,
+               email,
+               password,
+          },
+     });
 
-    if (!data.user) {
-        // throw new Error("Failed to register patient");
-        throw new AppError(status.BAD_REQUEST, "Failed to register patient");
-    }
+     if (!data.user) {
+          throw new AppError(status.BAD_REQUEST, "Failed to register patient");
+     }
 
-    //TODO : Create Patient Profile In Transaction After Sign Up Of Patient In USer Model
-    try {
-        const patient = await prisma.$transaction(async (tx) => {
+     try {
+          const patient = await prisma.$transaction(async (tx) => {
+               const patientTx = await tx.patient.create({
+                    data: {
+                         userId: data.user.id,
+                         name: payload.name,
+                         email: payload.email,
+                    },
+               });
 
-            const patientTx = await tx.patient.create({
-                data: {
-                    userId: data.user.id,
-                    name: payload.name,
-                    email: payload.email,
-                }
-            })
+               return patientTx;
+          });
 
-            return patientTx
-        })
+          const accessToken = tokenUtils.getAccessToken({
+               userId: data.user.id,
+               role: data.user.role,
+               name: data.user.name,
+               email: data.user.email,
+               status: data.user.status,
+               isDeleted: data.user.isDeleted,
+               emailVerified: data.user.emailVerified,
+          });
 
-        const accessToken = tokenUtils.getAccessToken({
-            userId: data.user.id,
-            role: data.user.role,
-            name: data.user.name,
-            email: data.user.email,
-            status: data.user.status,
-            isDeleted: data.user.isDeleted,
-            emailVerified: data.user.emailVerified,
-        });
+          const refreshToken = tokenUtils.getRefreshToken({
+               userId: data.user.id,
+               role: data.user.role,
+               name: data.user.name,
+               email: data.user.email,
+               status: data.user.status,
+               isDeleted: data.user.isDeleted,
+               emailVerified: data.user.emailVerified,
+          });
 
-        const refreshToken = tokenUtils.getRefreshToken({
-            userId: data.user.id,
-            role: data.user.role,
-            name: data.user.name,
-            email: data.user.email,
-            status: data.user.status,
-            isDeleted: data.user.isDeleted,
-            emailVerified: data.user.emailVerified,
-        });
-
-        return {
-            ...data,
-            accessToken,
-            refreshToken,
-            patient
-        }
-
-    } catch (error) {
-        console.log("Transaction error : ", error);
-        await prisma.user.delete({
-            where: {
-                id: data.user.id
-            }
-        })
-        throw error;
-    }
-
-}
+          return {
+               ...data,
+               accessToken,
+               refreshToken,
+               patient,
+          };
+     } catch (error) {
+          console.log("Transaction error : ", error);
+          await prisma.user.delete({
+               where: {
+                    id: data.user.id,
+               },
+          });
+          throw error;
+     }
+};
 
 interface ILoginUserPayload {
-    email: string;
-    password: string;
+     email: string;
+     password: string;
 }
 
 const loginUser = async (payload: ILoginUserPayload) => {
-    const { email, password } = payload;
+     const { email, password } = payload;
 
-    const data = await auth.api.signInEmail({
-        body: {
-            email,
-            password,
+     const data = await auth.api.signInEmail({
+          body: {
+               email,
+               password,
+          },
+     });
+
+     if (data.user.status === UserStatus.BLOCKED) {
+          throw new AppError(status.FORBIDDEN, "User is blocked");
+     }
+
+     if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+          throw new AppError(status.NOT_FOUND, "User is deleted");
+     }
+
+     const accessToken = tokenUtils.getAccessToken({
+          userId: data.user.id,
+          role: data.user.role,
+          name: data.user.name,
+          email: data.user.email,
+          status: data.user.status,
+          isDeleted: data.user.isDeleted,
+          emailVerified: data.user.emailVerified,
+     });
+
+     const refreshToken = tokenUtils.getRefreshToken({
+          userId: data.user.id,
+          role: data.user.role,
+          name: data.user.name,
+          email: data.user.email,
+          status: data.user.status,
+          isDeleted: data.user.isDeleted,
+          emailVerified: data.user.emailVerified,
+     });
+
+     return {
+          ...data,
+          accessToken,
+          refreshToken,
+     };
+};
+
+
+const getMe = async (user : IRequestUser) => {
+    const isUserExists = await prisma.user.findUnique({
+        where : {
+            id : user.userId,
+        },
+        include : {
+            patient : {
+                include : {
+                    appointments : true,
+                    reviews : true,
+                    prescriptions : true,
+                    medicalReports : true,
+                    patientHealthData : true,
+                }
+            },
+            doctor : {
+                include : {
+                    specialties : true,
+                    appointments : true,
+                    reviews : true,
+                    prescriptions : true,
+                }
+            },
+            admin : true,
         }
     })
 
-    if (data.user.status === UserStatus.BLOCKED) {
-        throw new AppError(status.FORBIDDEN, "User is blocked");
+    if (!isUserExists) {
+        throw new AppError(status.NOT_FOUND, "User not found");
     }
 
-    if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
-        throw new AppError(status.NOT_FOUND, "User is deleted");
-    }
-
-    const accessToken = tokenUtils.getAccessToken({
-        userId: data.user.id,
-        role: data.user.role,
-        name: data.user.name,
-        email: data.user.email,
-        status: data.user.status,
-        isDeleted: data.user.isDeleted,
-        emailVerified: data.user.emailVerified,
-    });
-
-    const refreshToken = tokenUtils.getRefreshToken({
-        userId: data.user.id,
-        role: data.user.role,
-        name: data.user.name,
-        email: data.user.email,
-        status: data.user.status,
-        isDeleted: data.user.isDeleted,
-        emailVerified: data.user.emailVerified,
-    });
-
-    return {
-        ...data,
-        accessToken,
-        refreshToken,
-    };
-
+    return isUserExists;
 }
 
+
 export const AuthService = {
-    registerPatient,
-    loginUser,
+     registerPatient,
+     loginUser,
+     getMe
 };
