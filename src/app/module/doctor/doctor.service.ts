cac11cae12c76f115c38c02459317fa74077288d
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-02-12 06:59:28 +0600

    add get doctor by id part

--- src/app/module/doctor/doctor.service.ts
+++ src/app/module/doctor/doctor.service.ts
@@ -1,19 +1,51 @@
 import { prisma } from "../../lib/prisma";
 
 const getAllDoctors = async () => {
-    const doctors = await prisma.doctor.findMany({
-        include: {
-            user: true,
-            specialties: {
-                include: {
-                    specialty: true
-                }
-            }
-        }
-    })
-    return doctors;
-}
+     const doctors = await prisma.doctor.findMany({
+          include: {
+               user: true,
+               specialties: {
+                    include: {
+                         specialty: true,
+                    },
+               },
+          },
+     });
+     return doctors;
+};
+
+const getDoctorById = async (id: string) => {
+     const doctor = await prisma.doctor.findUnique({
+          where: {
+               id,
+               isDeleted: false,
+          },
+          include: {
+               user: true,
+               specialties: {
+                    include: {
+                         specialty: true,
+                    },
+               },
+               appointments: {
+                    include: {
+                         patient: true,
+                         schedule: true,
+                         prescription: true,
+                    },
+               },
+               doctorSchedules: {
+                    include: {
+                         schedule: true,
+                    },
+               },
+               reviews: true,
+          },
+     });
+     return doctor;
+};
 
 export const DoctorService = {
-    getAllDoctors,
-}
+     getAllDoctors,
+     getDoctorById,
+};
