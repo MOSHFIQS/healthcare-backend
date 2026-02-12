f44313b88a3b9fad91a24d3755dcdbd1e9cce1af
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-02-12 06:47:18 +0600

    add admin create part

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
-import { createDoctorZodSchema } from "./user.validation";
+import { createAdminZodSchema, createDoctorZodSchema } from "./user.validation";
 import { validateRequest } from "../../middleware/validateRequest";
-
-
-
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
+router.post(
+     "/create-doctor",
+     validateRequest(createDoctorZodSchema),
+     UserController.createDoctor,
+);
 
-router.post("/create-doctor",
-    validateRequest(createDoctorZodSchema),
 
-    UserController.createDoctor);
+
+router.post("/create-admin",
+    // checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+    validateRequest(createAdminZodSchema),
+    UserController.createAdmin);
 
 export const UserRoutes = router;
